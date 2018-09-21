e49a037e4b0bdfd37b501e91472f17a0977caa7c
AndersSkovNielsen <anderssnielsen@live.dk>
2018-09-21 12:49:39 +0200

    Startet REST opgave

---
+++ ModelLibrary/Model/Data.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLibrary.Model
+{
+    public class Data
+    {
+        private int _a;
+        private int _b;
+
+        public int A
+        {
+            get { return _a; }
+            set { _a = value; }
+        }
+
+        public int B
+        {
+            get { return _b; }
+            set { _b = value; }
+        }
+
+        public Data()
+        {
+                
+        }
+
+        public Data(int a, int b)
+        {
+            _a = a;
+            _b = b;
+        }
+    }
+}
