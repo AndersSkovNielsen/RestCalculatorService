13d8dfbf03084bb0d8e17166a5e22ac47afbed8f
AndersSkovNielsen <anderssnielsen@live.dk>
2018-10-05 10:28:37 +0200

    Ændret string til enumerable af CalculationType

--- RESTCalculatorConsumer/Program.cs
+++ RESTCalculatorConsumer/Program.cs
@@ -7,29 +7,37 @@ using ModelLibrary.Model;
 
 namespace RESTCalculatorConsumer
 {
+    public enum CalculationType
+    {
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    }
     class Program
     {
         private const string CalculatorUri = "http://localhost:59226/api/Calculator/";
+        private CalculationType Type;
 
         //Consumer virker ikke ved almindelig start. Start service individuelt via Debug > New Instance først, derefter consumer.
         static void Main(string[] args)
         {
             Data d = new Data(25, 7);
-            Console.WriteLine(Async(d, "Add"));
+            Console.WriteLine(Async(d, CalculationType.Add));
             
             Data d2 = new Data(7, 5);
-            Console.WriteLine(Async(d2, "Subtract"));
+            Console.WriteLine(Async(d2, CalculationType.Subtract));
 
             Data d3 = new Data(20, 4);
-            Console.WriteLine(Async(d3, "Multiply"));
+            Console.WriteLine(Async(d3, CalculationType.Multiply));
 
             Data d4 = new Data(30, 3);
-            Console.WriteLine(Async(d4, "Divide"));
+            Console.WriteLine(Async(d4, CalculationType.Divide));
 
             Console.ReadLine();
         }
 
-        public static string Async(Data data, string calculationType)
+        public static string Async(Data data, CalculationType type)
         {
             using (HttpClient client = new HttpClient())
             {
@@ -46,7 +54,7 @@ namespace RESTCalculatorConsumer
                 //Vores URI bliver skrevet ud.
                 Console.WriteLine("CalculatorUri: " + CalculatorUri);
                 //Vores content bliver nu sendt afsted med Post, samt bliver "Add" tilføjet til vores URI.
-                HttpResponseMessage response = client.PostAsync(CalculatorUri + calculationType, content).Result;
+                HttpResponseMessage response = client.PostAsync(CalculatorUri + type, content).Result;
                 //Vi afventer svar for at få vores content til en string.
                 string str = response.Content.ReadAsStringAsync().Result;
                 //Vi deserialiserer vores str, så den ikke er json mere.
